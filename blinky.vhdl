692909668f98c1af4ffaa88d9edcae05b8a38560
Paul Gardner-Stephen <paul.gardner.stephen@gmail.com>
2025-08-06 14:05:53 +0930

    blink slower. echo on USB UART

--- blinky.vhdl
+++ blinky.vhdl
@@ -5,20 +5,23 @@ use ieee.numeric_std.all;
 entity blinky is
     port (
         CLK : in std_logic;
-        LED : out std_logic
+        LED : out std_logic;
+	TX : out std_logic;
+	RX : in std_logic
     );
 end entity;
 
 architecture rtl of blinky is
-    signal counter : unsigned(23 downto 0) := (others => '0');
+    signal counter : unsigned(27 downto 0) := (others => '0');
 begin
     process(clk)
     begin
         if rising_edge(clk) then
             counter <= counter + 1;
+		TX <= RX;
         end if;
     end process;
 
-    led <= counter(21);  -- slow blink
+    led <= counter(25);  -- slow blink
 end architecture;
 
